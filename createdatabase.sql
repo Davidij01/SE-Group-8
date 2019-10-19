6f1d3e16344a45d35c84237ab05f9eda51aa41dd
Davidij01 <56596641+Davidij01@users.noreply.github.com>
2019-10-19 17:20:08 -0400

    Add files via upload

---
+++ createdatabase.sql
@@ -0,0 +1,34 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `davidijagbemi` DEFAULT CHARACTER SET utf8 ;
+USE `davidijagbemi` ;
+
+-- -----------------------------------------------------
+-- Table `davidijagbemi`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `davidijagbemi`.`users` (
+  `id` INT NOT NULL,
+  `Firstname` VARCHAR(45) NOT NULL,
+  `Lastname` VARCHAR(45) NOT NULL,
+  `Username` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  `UserRole` VARCHAR(45) NOT NULL,
+  `Password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
